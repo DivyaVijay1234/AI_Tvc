dfac19586e24209324a5adde6570dbe335f04cea
anirudh11233 <156311792+anirudh11233@users.noreply.github.com>
2025-05-24 09:58:09 +0530

    Delete train_rl_agent.py

--- train_rl_agent.py
+++
@@ -1,9 +0,0 @@
-from stable_baselines3 import PPO
-from rocket_tvc_env import RocketTVCEnv
-
-env = RocketTVCEnv()
-
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=50000)
-
-model.save("ppo_tvc")
